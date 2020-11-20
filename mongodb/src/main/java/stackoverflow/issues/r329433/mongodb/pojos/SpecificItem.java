a47e312daa54187613c5aa189a4bdfc19b4013d2
Joneko <18681252+J0NEK0@users.noreply.github.com>
2020-11-20 19:47:44 +0100

    Update SpecificItem.java

--- mongodb/src/main/java/stackoverflow/issues/r329433/mongodb/pojos/SpecificItem.java
+++ mongodb/src/main/java/stackoverflow/issues/r329433/mongodb/pojos/SpecificItem.java
@@ -1,5 +1,6 @@
 package stackoverflow.issues.r329433.mongodb.pojos;
 
+import org.bson.codecs.pojo.annotations.BsonCreator;
 import org.bson.codecs.pojo.annotations.BsonDiscriminator;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
@@ -8,6 +9,7 @@ public class SpecificItem extends GenericItem<StringID> {
 
 	private String genericProperty;
 
+	@BsonCreator
 	public SpecificItem(@BsonProperty("id") StringID id, @BsonProperty("genericProperty") String genericProperty) {
 		super(id, genericProperty);
 		this.genericProperty = genericProperty;
